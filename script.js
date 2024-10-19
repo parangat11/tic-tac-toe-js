576581cdd5aaaa1e3883dd025707cfbfaa390296
parangat11 <parangataditya001@gmail.com>
2024-10-19 06:20:12 +0530

    Complete the logic for console version of tic-tac-toe.
    Game is playable on console now.

--- script.js
+++ script.js
@@ -23,11 +23,7 @@ const gameboard = (function() {
 
     // To add the token of player to the cell.
     const addToken = (row, col, player) => {
-        if(board[row][col] === 0) {
-            board[row][col] = player;
-            return true;
-        }
-        return false;
+        return board[row][col] === 0;
     }
 
     return {getBoard, addToken};
@@ -56,13 +52,22 @@ function GameController() {
     const play = () => {
         const board = gameboard.getBoard();
         while(1) {
-            const currPlayer = getActivePlayer();
-            const row = Number(prompt('Enter row'));
-            const col = Number(prompt('Enter column'));
+            const currPlayer = Number(getActivePlayer());
+            console.table(board);
+            console.log(currPlayer);
+            let row = Number(prompt('Enter row'));
+            let col = Number(prompt('Enter column'));
+
+            // 0-based indexing
+            row--;
+            col--;
             if(gameboard.addToken(row, col, currPlayer)) {
+                board[row][col] = currPlayer + 1;
                 if(checkWinner(board)) {
-                    console.log('Winner is player ' + players[currPlayer].name);
+                    console.log('Winner is player ' + currPlayer);
+                    break;
                 }
+                console.table(board);
                 switchActivePlayer();
             }
         }
@@ -73,6 +78,8 @@ function GameController() {
 
         // Check rows
         for(let i = 0; i < 3; i++) {
+            if(board[i][0] === 0)
+                break;
             let val = board[i][0];
             let curr = true;
             for(let j = 0; j < 3; j++) {
@@ -85,6 +92,8 @@ function GameController() {
 
         // Check cols
         for(let i = 0; i < 3; i++) {
+            if(board[0][i] === 0)
+                break;
             let val = board[0][i];
             let curr = true;
             for(let j = 0; j < 3; j++) {
@@ -98,7 +107,7 @@ function GameController() {
         // Check diagonal-1
         let curr = true;
         for(let i = 0; i < 3; i++) {
-            if(board[i][i] != board[0][0])
+            if(board[i][i] != board[0][0] || board[i][i]===0)
                 curr = false;
         }
         winner |= curr;
@@ -106,13 +115,15 @@ function GameController() {
         // Check diagonal-2
         curr = true;
         for(let i = 0; i < 3; i++) {
-            if(board[i][2 - i] != board[0][2])
+            if(board[i][2 - i] != board[0][2] ||board[0][2]===0)
                 curr = false;
         }
         winner |= curr;
 
         return winner;
     }
+
+    return {play};
 }
 
 const newGame = GameController();
